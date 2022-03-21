f4dd8947f862765dae173cf22117e7f14450bfde
ezepersos <ezequielperez50@gmail.com>
2022-03-21 11:34:31 +0100

    Añadidos los atributos faltantes
    A falta de añadir a los objetos de la BD y testear metodos

--- src/main/java/org/springframework/samples/petclinic/restaurants/Restaurant.java
+++ src/main/java/org/springframework/samples/petclinic/restaurants/Restaurant.java
@@ -15,11 +15,14 @@
  */
 package org.springframework.samples.petclinic.restaurants;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import org.springframework.samples.petclinic.model.NamedEntity;
@@ -54,5 +57,8 @@ public class Restaurant extends NamedEntity {
 	@Column(name = "photo")
 	private String photo;
 	
+	@Column(name = "type")
+	@NotNull
+	private List<RestaurantType> type;
 
 }
--- src/main/java/org/springframework/samples/petclinic/restaurants/RestaurantRepository.java
+++ src/main/java/org/springframework/samples/petclinic/restaurants/RestaurantRepository.java
@@ -18,6 +18,12 @@ public interface RestaurantRepository extends Repository<Restaurant, Integer> {
 
 	public List<Restaurant> findAll();
 
+	@Query("SELECT restaurant FROM Restaurant restaurant WHERE restaurant.type =:type")
+	public List<Restaurant> findByType(@Param("type") RestaurantType type);
+
+	@Query("SELECT restaurant FROM Restaurant restaurant WHERE restaurant.city =:city")
+	public List<Restaurant> findByCity(@Param("city") String city);
+
 	void deleteById(int id) throws DataAccessException;
 
 }
