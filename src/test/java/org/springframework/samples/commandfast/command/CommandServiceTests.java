ac62611f3ea5d9ef3612f3bfb92db64e9ddd2242
José Antonio <josdomqui@alum.us.es>
2022-03-23 21:06:45 +0100

    HotFix Test

--- src/test/java/org/springframework/samples/commandfast/command/CommandServiceTests.java
+++ src/test/java/org/springframework/samples/commandfast/command/CommandServiceTests.java
@@ -1,21 +1,5 @@
-///*
-// * Copyright 2002-2013 the original author or authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *      http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
 package org.springframework.samples.commandfast.command;
-//
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collection;
@@ -28,38 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.commandfast.mesa.Mesa;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//
-///**
-// * Integration test of the Service and the Repository layer.
-// * <p>
-// * ClinicServiceSpringDataJpaTests subclasses benefit from the following
-// * services provided by the Spring TestContext Framework:
-// * </p>
-// * <ul>
-// * <li><strong>Spring IoC container caching</strong> which spares us unnecessary
-// * set up time between test execution.</li>
-// * <li><strong>Dependency Injection</strong> of test fixture instances, meaning
-// * that we don't need to perform application context lookups. See the use of
-// * {@link Autowired @Autowired} on the <code>{@link
-// * PlayerServiceTests#clinicService clinicService}</code> instance variable,
-// * which uses autowiring <em>by type</em>.
-// * <li><strong>Transaction management</strong>, meaning each test method is
-// * executed in its own transaction, which is automatically rolled back by
-// * default. Thus, even if tests insert or otherwise change database state, there
-// * is no need for a teardown or cleanup script.
-// * <li>An {@link org.springframework.context.ApplicationContext
-// * ApplicationContext} is also inherited and can be used for explicit bean
-// * lookup if necessary.</li>
-// * </ul>
-// *
-// * @author Ken Krebs
-// * @author Rod Johnson
-// * @author Juergen Hoeller
-// * @author Sam Brannen
-// * @author Michael Isvy
-// * @author Dave Syer
-// */
-//
+
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 class CommandServiceTests {
 	@Autowired
