b14c88182d708eb4b247b2bbb0f733506f48329a
[marmarave] <[marmarave@alum.us.es]>
2022-04-06 20:04:44 +0200

    Arreglo en los test

--- src/test/java/org/springframework/samples/commandfast/payment/PaymenttServiceTests.java
+++ src/test/java/org/springframework/samples/commandfast/payment/PaymenttServiceTests.java
@@ -1,22 +1,15 @@
 package org.springframework.samples.commandfast.payment;
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-import javax.persistence.EntityManager;
 
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.samples.commandfast.mesa.Mesa;
 import org.springframework.samples.commandfast.payments.Payment;
 import org.springframework.samples.commandfast.payments.PaymentService;
-import org.springframework.samples.commandfast.product.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.commandfast.restaurantes.Restaurante;
-import org.springframework.samples.commandfast.restaurantes.RestauranteService;
-import org.springframework.samples.commandfast.restaurantes.RestauranteType;
-import org.springframework.samples.commandfast.user.User;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -28,17 +21,44 @@ public class PaymenttServiceTests {
     
     @Test
     void shouldFindAllPayments(){
+    Mesa m = new Mesa();
+    m.setCostumer(5);
+    m.setId(1);
+    m.setNumber(50);
+    
+    Payment pa = new Payment();
+    pa.setAmount(25.0);
+    pa.setCreditCard(Boolean.FALSE);
+    pa.setDate(LocalDate.now());
+    pa.setId(1);
+    pa.setPayHere(Boolean.TRUE);
+    pa.setTable(m);
+    this.paymentService.savePayment(pa);
     List<Payment> p = this.paymentService.getAllPayments();
-    assertThat(p.size()!=0);
+
+    assertThat(p.isEmpty()).isFalse();
     }
 
     @Test
     void shouldFindPaymentsById(){
-    assertThat(this.paymentService.getPaymentById(1));
+        Mesa m = new Mesa();
+    m.setCostumer(5);
+    m.setId(1);
+    m.setNumber(50);
+    
+    Payment pa = new Payment();
+    pa.setAmount(25.0);
+    pa.setCreditCard(Boolean.FALSE);
+    pa.setDate(LocalDate.now());
+    pa.setId(1);
+    pa.setPayHere(Boolean.TRUE);
+    pa.setTable(m);
+    this.paymentService.savePayment(pa);
+    assertThat(this.paymentService.getPaymentById(pa.getId()).get().getId()).isEqualTo(pa.getId());
     }
 
     @Test
     void shouldGeneratePayment(){
-    assertThat(this.paymentService.generateRecipt(2.5));
+    assertThat(this.paymentService.generateRecipt(2.5)).isNotEmpty();
     }
 }
