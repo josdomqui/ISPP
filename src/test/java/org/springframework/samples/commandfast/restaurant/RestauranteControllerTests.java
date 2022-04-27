1521028a38c0039f41b8ca29b70604dccda298c8
[marmarave] <[marmarave@alum.us.es]>
2022-04-27 18:46:21 +0200

    Añadido algunos test

--- src/test/java/org/springframework/samples/commandfast/restaurant/RestauranteControllerTests.java
+++ src/test/java/org/springframework/samples/commandfast/restaurant/RestauranteControllerTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.commandfast.restaurantes.RestauranteController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -102,4 +106,30 @@ class RestauranteControllerTests {
 
     }
 
+    @WithMockUser
+    @Test
+    void testDeberiaMostrarFormularioRestaurante() throws Exception{
+        mockMvc.perform(get("/restaurante/signup", 1)).andExpect(status().isOk()).andExpect(view().name("restaurantes/createRestaurantForm"));
+
+    }
+    
+
+    @WithMockUser
+    @Test
+    void testNotif() throws Exception{
+        mockMvc.perform(get("/restaurante/notify/3", 1)).andExpect(status().isOk());
+
+    }
+
+
+    
+    @WithMockUser
+    @Test
+    void testNoDeberiaEliminarProducto() throws Exception{
+        mockMvc.perform(delete("/restaurante/3/detalles/carta")).andExpect(status().isForbidden());
+    }
+
+
+    
 }
+
